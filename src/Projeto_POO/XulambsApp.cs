95a94540dcf597878ef598775b8f03642640545f
Joao Caram <caram@pucminas.br>
2026-09-04 18:55:58 -0300

    XulambsApp: v2

--- src/Projeto_POO/XulambsApp.cs
+++ src/Projeto_POO/XulambsApp.cs
@@ -3,74 +3,152 @@
 namespace XulambsFoods {
     public class XulambsApp
     {
-        static LinkedList<Pizza> pizzas;
-        
-        static void Cabecalho() {
-            Console.Clear();
-            Console.WriteLine("XULAMBS PIZZA v0.11");
-            Console.WriteLine("==================");
-            Console.WriteLine($"Pizzas vendidas hoje: {Pizza.GetQuantidadeVendida():D2}");
-        }
+        static LinkedList<Pedido> pedidos;
+static string versao = "0.2";
 
-        static void Pausa() {
-            Console.WriteLine("Digite <ENTER> para continuar.");
-            Console.ReadLine();
-        }
+#region utilidades
+static void Pausa() {
+    Console.WriteLine("\nDigite enter para continuar.");
+    Console.ReadKey();
+}
+static void Cabecalho() {
+    Console.Clear();
+    Console.WriteLine($"Xulambs Pizza v{versao}");
+    Console.WriteLine("===================");
+}
+#endregion
 
-        static int MenuPrincipal() {
-            Cabecalho();
-            Console.WriteLine("1 - Comprar uma pizza");
-            Console.WriteLine("2 - Mostrar pizzas vendidas");
-            Console.WriteLine("0 - Sair");
-            Console.Write("Sua opção: ");
-            return int.Parse(Console.ReadLine());
-        }
+#region configuração
+static void Config() {
+    pedidos = new LinkedList<Pedido>();
+}
 
-        static void ImprimirDadosPizza(Pizza pizza) {
-            Console.WriteLine();
-            Console.WriteLine("Pizza comprada:\n ");
-            Console.WriteLine(pizza.GerarCupom());
-        }
+#endregion
+#region menus
+static int MenuPrincipal() {
+    Cabecalho();            
+    StringBuilder menu = new StringBuilder();
+    menu.AppendLine("1 - Abrir um pedido");
+    menu.AppendLine("2 - Relatório de um pedido");
+    menu.AppendLine("3 - Incluir pizza em pedido");
+    menu.AppendLine("4 - Fechar pedido");
+    menu.AppendLine("0 - Finalizar");
+    menu.Append("Sua opção: ");
+    Console.Write(menu.ToString());
+    return int.Parse(Console.ReadLine());
+}
+#endregion
 
-        static void ComprarPizza() {
-            Cabecalho();
-            Console.WriteLine("Comprando uma pizza:");
-            Console.Write("Quantos ingredientes você deseja (0-8)? ");
-            int quantos = int.Parse(Console.ReadLine());
-            Pizza novaPizza = new Pizza();
-            novaPizza.AdicionarIngredientes(quantos);
-            ImprimirDadosPizza(novaPizza);
-            pizzas.AddLast(novaPizza);
+#region relatorios
+static void ImprimirDadosPedido(Pedido pedido) {
+    Cabecalho();
+    string msg = pedido?.Relatorio() ?? "Pedido não encontrado.";
+    Console.WriteLine(msg);
+}
 
-        }
+static void ImprimirDadosPizza(Pizza pizza) {
+    Console.WriteLine();
+    Console.WriteLine("Pizza comprada:\n ");
+    Console.WriteLine(pizza.GerarCupom());
+    Pausa();
+}
+#endregion
 
-        private static void MostrarPizzas() {
-            Cabecalho();
-            Console.WriteLine($"Pizzas vendidas até agora ({pizzas.Count}) pizzas:");
-            foreach (Pizza pizza in pizzas) {
-                ImprimirDadosPizza(pizza);
-            }
-        }
+#region pizza
+static Pizza ComprarPizza() {
+    Cabecalho();
 
-        static void Main(string[] args) {
-            int opcao;
-            pizzas = new LinkedList<Pizza>();
-            
-            do {
-                opcao = MenuPrincipal();
-                Action metodo = 
-                opcao switch {
-                    1 => () => ComprarPizza(),
-                    2 => () => MostrarPizzas(),
-                    0 => () => Console.WriteLine("Encerrando!!"),
-                    _ => () => Console.WriteLine("Opção inválida."),
-                };
-                metodo.Invoke();
-                Pausa();
+    Console.WriteLine("Comprando uma pizza:");
+    Console.Write("Quantos ingredientes você deseja (0-8)? ");
+    int quantos = int.Parse(Console.ReadLine());
 
-            } while (opcao != 0);
-        }
+    Pizza novaPizza = new Pizza(quantos);
+    
+    ImprimirDadosPizza(novaPizza);
 
+    return novaPizza;
+}
+
+#endregion
+
+#region pedido
+static void CriarPedido() {
+    string resp = "s";
+    Pedido novoPedido = new Pedido();
+    do {
+        Pizza novaPizza = ComprarPizza();
+        novoPedido.Adicionar(novaPizza);
+        Console.Write("\n\nMais pizza? ");
+        resp = Console.ReadLine();
+    } while (resp.ToLower().Equals("s"));
+    pedidos.AddLast(novoPedido);
+    ImprimirDadosPedido(novoPedido);
+}
+
+
+
+static void RelatorioPedido() {
+    Cabecalho();
+    Console.Write("Número do pedido: ");
+    int codigo = int.Parse(Console.ReadLine());
+    ImprimirDadosPedido(LocalizarPedido(codigo));
+}
+
+
+static void FecharPedido() {
+    Cabecalho();
+    Console.Write("Número do pedido: ");
+    int codigo = int.Parse(Console.ReadLine());
+
+    Pedido pedido = LocalizarPedido(codigo);
+    pedido?.FecharPedido();
+    ImprimirDadosPedido(pedido);
+    
+}
+
+static void AlterarPedido() {
+    Cabecalho();
+    Pizza novaPizza = ComprarPizza();
+
+    Console.Write("Número do pedido: ");
+    int codigo = int.Parse(Console.ReadLine());
+
+    Pedido pedido = LocalizarPedido(codigo);
+    if (pedido != null)
+        pedido.Adicionar(novaPizza);
+
+    ImprimirDadosPedido(pedido);
+    
+}
+
+static Pedido LocalizarPedido(int codigo) {
+    Pedido localizado = null;
+    for (int i = 0; i < pedidos.Count && localizado == null; i++) {
+        Pedido candidato = pedidos.ElementAt(i);
+        if (candidato.GetID() == codigo)
+            localizado = candidato;
+    }
+    return localizado;
+}
+#endregion
+
+static void Main(string[] args) {
+    int opcao;
+    Config();
+    do {
+        opcao = MenuPrincipal();
+        Action ac =
+        opcao switch {
+            1 => () => CriarPedido(),
+            2 => () => RelatorioPedido(),
+            3 => () => AlterarPedido(),
+            4 => () => FecharPedido(),
+            _ => () => Pausa()
+        };
+        ac.Invoke();
+        Pausa();
+    } while (opcao != 0);
+}
        
     }
 }
